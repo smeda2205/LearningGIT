ea8ac645b7d88b1b4e3fa515a9dd51cd595a3665
Srinivasa Kumar Meda <sree.meda@gmail.com>
2013-08-08 15:50:11 -0700

    changed output message to know how to commit.

--- LearnGIT/src/com/learn/git/GitTest.java
+++ LearnGIT/src/com/learn/git/GitTest.java
@@ -2,6 +2,6 @@ package com.learn.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello GIT!!");
+		System.out.println("Hello GIT!! Updating the statement!!");
 	}
 }
